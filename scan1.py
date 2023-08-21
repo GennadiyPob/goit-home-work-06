882e008f2702925bbfa55d7a2fdcaf6c5cce186b
GennadiyPob <137322270+GennadiyPob@users.noreply.github.com>
2023-08-21 15:43:53 +0300

    Update

--- scan1.py
+++
@@ -1,8 +0,0 @@
-file_list = ['file1.txt', 'file2.txt', 'file3.txt']  # Список файлів, які потрібно перемістити
-destination_folder = 'destination_folder'  # Папка, куди потрібно перемістити файли
-
-for file in file_list:
-    source_path = file  # Шлях до вихідного файлу (в даному випадку в поточній директорії)
-    destination_path = f"{destination_folder}/{file}"  # Шлях до папки призначення разом з іменем файлу
-    shutil.move(source_path, destination_path)
-    print(f"Файл '{file}' переміщено до папки '{destination_folder}'.")
